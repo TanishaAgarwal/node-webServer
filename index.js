b1d5b8f7d4d65ee94b987f942f72f9d9739479d8
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-05-30 11:09:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app =express();
+
+app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/contact',function(req,res){
+    res.sendFile(__dirname + '/contact.html');
+});
+
+/*    1:"max",
+    2:"jack",
+    3:"ryu",
+    4:"han",
+    5:"john"
+};*/
+
+app.get('/profile/:name',function(req,res){
+    var data ={age:29, job:'teacher'};
+    res.render('profile',{person: req.params.name, data:data});
+});
+
+app.listen(3000);
